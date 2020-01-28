a2ecc43bbff2754433945851068fa5acec1db9c8
Jörg Richter <jri@deepamehta.de>
2020-01-28 14:07:34 +0100

    Fix `Geomap` instantiation, 3rd try
    See dmx-platform/dmx-platform#260

--- src/main/js/geomaps-service.js
+++ src/main/js/geomaps-service.js
@@ -1,26 +1,5 @@
 export default function GeomapsSerive (dm5, http) {
 
-  const Geomap = class extends dm5.Topic {
-
-    constructor (geomap) {
-      super(geomap.topic)
-      this.viewProps = geomap.viewProps
-      this.geoCoordTopics = geomap.geoCoordTopics     // instantiating dm5.Topic objects not required at the moment
-    }
-
-    removeTopic (id) {
-      // TODO; skeleton needed by dm5-topicmap-panel contract
-    }
-
-    removeAssoc (id) {
-      // TODO; skeleton needed by dm5-topicmap-panel contract
-    }
-  }
-
-  console.log('Instantiating GeomapsSerive')
-
-  // Service
-
   this.getGeomap = geomapId => {
     return http.get(`/geomap/${geomapId}`).then(response =>
       new Geomap(response.data)
@@ -40,4 +19,23 @@ export default function GeomapsSerive (dm5, http) {
     console.log('setGeomapState', lon, lat, zoom)
     http.put(`/geomap/${geomapId}/center/${lon}/${lat}/zoom/${zoom}`)
   }, 3000)
+
+  //
+
+  class Geomap {
+
+    constructor (geomap) {
+      this.id = geomap.topic.id
+      this.viewProps = geomap.viewProps
+      this.geoCoordTopics = geomap.geoCoordTopics     // instantiating dm5.Topic objects not required at the moment
+    }
+
+    removeTopic (id) {
+      // TODO; skeleton needed by dm5-topicmap-panel contract
+    }
+
+    removeAssoc (id) {
+      // TODO; skeleton needed by dm5-topicmap-panel contract
+    }
+  }
 }
