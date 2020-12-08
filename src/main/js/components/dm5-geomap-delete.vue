1c9bccf54367ee8ca20fa53d75b0821c9d5afe2a
Jörg Richter <jri@deepamehta.de>
2020-12-08 17:46:38 +0100

    Adapt to dmx-api (`restClient` -> `rpc`)

--- src/main/js/components/dm5-geomap-delete.vue
+++ src/main/js/components/dm5-geomap-delete.vue
@@ -20,7 +20,7 @@ export default {
         // update client state + sync view (for immediate visual feedback)
         this.$store.dispatch('_deleteTopic', this.topicmapId)
         // update server state
-        this.dm5.restClient.deleteTopic(this.topicmapId).then(response => {
+        this.dm5.rpc.deleteTopic(this.topicmapId).then(response => {
           this.$store.dispatch('_processDirectives', response.directives)
         })
       }).catch(() => {})    // suppress unhandled rejection on Cancel
--- src/main/js/components/dm5-geomap-renderer.vue
+++ src/main/js/components/dm5-geomap-renderer.vue
@@ -134,7 +134,7 @@ export default {
 
     showDetails (topic) {
       this.loading = true
-      this.dm5.restClient.getTopic(topic.id, true, true).then(topic => {
+      this.dm5.rpc.getTopic(topic.id, true, true).then(topic => {
         this.domainTopic = topic
         this.loading = false
         this.updatePopup()
